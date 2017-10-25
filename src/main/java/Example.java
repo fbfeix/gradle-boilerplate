a2bcfabcfa34ad5fa8ee04985183f98e30be63c9
Felix Astner <astner.felix@live.de>
2017-10-25 18:31:58 +0200

    init

---
+++ src/main/java/Example.java
@@ -0,0 +1,6 @@
+public class Example {
+
+    public boolean functionToTest() {
+        return true;
+    }
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void functionToTest() throws Exception {
+        final Example example = new Example();
+        assertTrue(example.functionToTest());
+    }
+
+}
